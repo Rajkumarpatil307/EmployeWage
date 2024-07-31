d9fb70195ad59ab5fb19d718b96941a71f8d5180
Rajkumarpatil307 <patil.rajkumar307@gmail.com>
2024-07-31 11:44:12 +0530

    Employee Wage  welcome message

---
+++ com/First/EmployeWageComputation.java
@@ -0,0 +1,8 @@
+package com.First;
+
+public class EmployeWageComputation {
+	public static void main(String agrs[]) {
+		System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+	}
+
+}
